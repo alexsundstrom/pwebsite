9a02ae10ff8e2132a68c28da42b466bed6dec12d
alexsundstrom <alexandersundstrom1@hotmail.com>
2020-08-27 08:55:23 +0200

    added fontawesomeicons and imports

---
+++ src/components/FontAwesomeIcons.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
+
+
+
+library.add(
+    faLinkedin,
+    faGithub
+    
+  );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 function Footer() {
 
@@ -10,15 +13,18 @@ function Footer() {
             <Container fluid={true}>
                 <Row className="border-top p-3">
                         <Col className="p-0" md={11} xs={6}>
-                        Linkedin
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} size="lg" />
+                        
                         </Col>
                         <Col className="p-0" md={1} xs={6}>
                         ©Alexander 2020
                         </Col>
                     
                         <Col className="p-0" md={4} xs={1}>
-                        Github
+                        <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
+                        
                         </Col>
+                        
                 </Row>
             </Container>
         </footer>
